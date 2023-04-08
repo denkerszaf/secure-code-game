7ea90bb234bf835f13cff91c6f86c1c4739aa6cc
Stefan Denker <Stefan@dn-kr.de>
2023-04-08 16:07:30 +0200

    Limit acceptance of payments and purchases

--- Level-1/code.py
+++ Level-1/code.py
@@ -16,18 +16,23 @@ from collections import namedtuple
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+MAX_QUANTITY = 1000
+MAX_PAYMENT = 10e6
+MAX_AMOUNT = 100000
+
 def validorder(order: Order):
-    net = 0
+    net = 0.0
     
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            net += max(min(item.amount,MAX_PAYMENT),-1 * MAX_PAYMENT) 
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if (item.quantity < MAX_QUANTITY and item.amount < MAX_AMOUNT):
+                net -= item.amount * item.quantity
         else:
             return("Invalid item type: %s" % item.type)
     
-    if net != 0:
+    if net != 0.0:
         return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
         return("Order ID: %s - Full payment received!" % order.id)
